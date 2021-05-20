06d823978dad837494ec90d4689698f884fd80f8
launchpad5682 <launchpad5682@gmail.com>
2021-05-20 14:45:00 +0530

    Updated the API code

--- PyMongoFlaskAPI/app.py
+++ PyMongoFlaskAPI/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from json import dumps
-import json
-from bson import objectid
-from json import JSONEncoder
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from pymongo.message import query
 
 app = Flask("to-do-api")
 app.config['MONGO_URI'] = 'mongodb://172.17.0.2:27017/to-do-app'
@@ -12,11 +12,7 @@ app.config['MONGO_URI'] = 'mongodb://172.17.0.2:27017/to-do-app'
 database = PyMongo(app)
 collection = database.db.tasks
 
-
-def read_one(collection, query):
-    doc_id = collection.find_one(query)
-    return doc_id
-
+# Schema includes the task and done bool to check for the task is done or not.
 
 @app.route('/add_task', methods=['POST'])
 def add_task():
@@ -24,42 +20,61 @@ def add_task():
     print(task_data)
     # insert data
     collection.insert_one(task_data).inserted_id
-    return "Done", 201
+    response = jsonify("Done")
+    response.status_code = 201
+    return response
 
 
 @app.route('/tasks', methods=['GET'])
 def show_tasks():
-    tasks = []
     documents = collection.find()
-    #list_document = list(documents)
-    #json_tasks = dumps(list_document)
-    # print(json_tasks)
-    #test = jsonify({"tasks":tasks})
-    for document in documents:
-        doc = {'name': document['name'], 'class': document['class']}
-        tasks.append(doc)
-        #tasks.append(json.encode(document, cls=JSONEncoder))
-
-    #print(tasks)
-    return jsonify(tasks)
+    response = jsonify(dumps(documents))
+    response.status_code = 200
+    return response
 
 
-@app.route('/delete_one', methods=['DELETE'])
-def delete_one():
-    task = request.json
-    print(task)
-    delete = collection.delete_one(task)
+@app.route("/show_task/<id>", methods=['GET'])
+def show_task(id):
+    document = collection.find_one({'_id': ObjectId(id)})
+    response = jsonify(dumps(document))
+    response.status_code = 200
+    return response
+
+
+@app.route('/delete_one/<id>', methods=['DELETE'])
+def delete_one(id):
+    delete = collection.delete_one({'_id': ObjectId(id)})
     print(delete)
-    return "Deleted", 204
+    response = jsonify("Deleted {} successfully".format(id))
+    response.status_code = 200
+    return response
+
 
 @app.route('/delete_all', methods=['DELETE'])
 def delete_all():
     delete = collection.delete_many({})
     print(delete)
-    return "Deleted all",204
+    response = jsonify("Deleted all the tasks successfully")
+    response.status_code = 204
+    return response
 
-@app.route('/update_task', methods=['PUT'])
-def update_task():
-    return None
+
+@app.route('/update_task/<id>', methods=['PUT'])
+def update_task(id):
+    query = request.json
+    print(query)
+    task = query['task']
+    done = query['done']
+    if task and done and id and request.method == 'PUT':
+        update = collection.update_one({'_id': ObjectId(id['$oid']) if '$oid' in id else ObjectId(id)}, {
+            '$set': {'task': task, 'class': done}})
+        print(update)
+        response = jsonify("Update Successful")
+        response.status_code = 200
+        return response
+
+    else:
+        return jsonify("Error")
+
 
 app.run(port=5555, debug=True)
